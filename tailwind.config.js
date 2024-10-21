8ad5b56c17d62b908741e95fe0e717eab2038033
AGiesa <anette.giesa@gmail.com>
2024-10-21 22:44:33 +0200

    lib active menu button state

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,21 @@ module.exports = {
       fontFamily: {
         'sans': ['Saira', ...defaultTheme.fontFamily.sans],
       },
-      extend: {},
+      extend: {
+        colors: {
+          'petrol-100':'#E3F5FA',
+          'petrol-200':'#C7E7F0',
+          'petrol-300':'#86DAF0',
+          'petrol-400':'#4ABCD9',
+          'petrol-500':'#2797B2',
+          'petrol-600':'#016C88',
+          'petrol-700':'#0F5364',
+          'petrol-800':'#114552',
+
+        }
+      },
     },
+
 
   variants: {
     extend: {
