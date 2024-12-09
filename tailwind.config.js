4f9fd25f5dbc91a0a04c30a318fbbd0b23c432e2
AGiesa <anette.giesa@gmail.com>
2024-12-09 22:04:49 +0100

    togg2

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./public/**/*.{html,js}",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
+    "./src/**/*.js"
   ],
   
   darkMode: 'class',
@@ -15,14 +16,18 @@ module.exports = {
 
       extend: {
         colors: {
-          'petrol-100':'#E3F5FA',
-          'petrol-200':'#C7E7F0',
-          'petrol-300':'#86DAF0',
-          'petrol-400':'#4ABCD9',
-          'petrol-500':'#2797B2',
-          'petrol-600':'#016C88',
-          'petrol-700':'#0F5364',
-          'petrol-800':'#114552',
+          'petrol':{
+          100:'#E3F5FA',
+          200:'#C7E7F0',
+          300:'#86DAF0',
+          400:'#4ABCD9',
+          500:'#2797B2',
+          600:'#016C88',
+          700:'#0F5364',
+          800:'#114552',
+          /**'primary-1': "var(--primary-1)",
+          'primary-2': "var(--primary-2)",
+          'primary-3': "var(--primary-3)",*/
 
         }
       },
@@ -36,9 +41,9 @@ module.exports = {
       ringColor: ['focus'],
     },
   },
-
+},
   plugins: [
     require('flowbite/plugin')
     
   ],
-}
+} 
